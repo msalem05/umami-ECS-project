224b63c03ce2e6679356c07c09d9ee6459d26a6d
msalem05 <salemmohammed571@gmail.com>
2025-09-09 12:14:48 +0100

    completed vpc main.tf and variables.tf

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,75 @@
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.main.id 
+    count = length(var.az)
+    cidr_block = var.public_subnet_cidr[count.index]
+    availability_zone = var.az[count.index]
+    map_public_ip_on_launch = true
+
+}
+
+resource "aws_subnet" "private" {
+    vpc_id = aws_vpc.main.id 
+    count = length(var.az)
+    cidr_block = var.private_subnet_cidr[count.index]
+    availability_zone = var.az[count.index]
+
+}
+
+resource "aws_eip" "eip" {
+}
+
+resource "aws_nat_gateway" "nat" {
+    allocation_id = aws_eip.eip.id
+    subnet_id = aws_subnet.private.id
+
+    depends_on = [ aws_internet_gateway.example ]
+}
+
+resource "aws_nat_gateway_eip_association" "nat_eip" {
+    allocation_id = aws_eip.eip.id
+    nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+}
+
+resource "aws_route" "public_route" {
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_association" {
+    route_table_id = aws_route_table.public.id
+    subnet_id = aws_subnet.public.id
+}
+
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+
+}
+
+resource "aws_route" "private_route" {
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = var.vpc_cidr
+    nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_association" {
+    route_table_id = aws_route_table.private.id
+    subnet_id = aws_subnet.private.id
+}
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    type = list(string)
+    default = ["10.0.1.0/24" , "10.0.2.0/24"]
+}
+
+variable "az" {
+    type = list(string)
+    default = ["eu-west-2a","eu-west-2b"]
+}
+
+variable "private_subnet_cidr" {
+    type = list(string)
+    default = ["10.0.3.0/24","10.0.4.0/24"]
+}
